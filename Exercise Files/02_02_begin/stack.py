942ec061b3720ea5cbd6a98799db0f0763c7e21d
Stephan DuVal <stephan.duval@gmail.com>
2023-01-24 11:24:10 -0700

    Finishes Section 2.2

--- Exercise Files/02_02_begin/stack.py
+++ Exercise Files/02_02_begin/stack.py
@@ -17,4 +17,32 @@ class Stack:
         self.items.append(item)
 
     def pop(self):
-        self.items.pop()
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+
+# If this is the main file being run (IE its not imported then execute the following code, otherwise execute the class called "Stack")
+if __name__ == "__main__":
+    s = Stack()   # call the constructor of the stack
+
+    print(s)
+    print(s.is_empty())
+    s.push(3)
+    s.push(5)
+    print(s)
+    s.push(7)
+
+    print(s)
+    print(s.pop())
+    print(s)
+    print(s.peek())
+    print(s.size())
+    print(s.is_empty())
