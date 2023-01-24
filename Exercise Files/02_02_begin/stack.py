c504144634d91f908ae7bd4906a73e327e4101ba
Stephan DuVal <stephan.duval@gmail.com>
2023-01-24 11:08:02 -0700

    Lesson 2.2

--- Exercise Files/02_02_begin/stack.py
+++ Exercise Files/02_02_begin/stack.py
@@ -6,4 +6,15 @@ Robin Andrews - https://compucademy.net/
 
 
 class Stack:
-    pass
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        # return len(self.items) == 0
+        return not self.items
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
