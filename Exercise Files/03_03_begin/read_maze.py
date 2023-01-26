0900052f45696641c248cfd5bff2b1c4f2513c7e
Stephan DuVal <stephan.duval@gmail.com>
2023-01-26 12:14:33 -0700

    Finished lesson 3.2

--- Exercise Files/03_03_begin/read_maze.py
+++ Exercise Files/03_03_begin/read_maze.py
@@ -21,3 +21,9 @@ def read_maze(file_name):
     except OSError:
         print("There is a problem with the file you have selected.")
         raise SystemExit
+
+
+if __name__ == "__main__":
+    maze = read_maze("mazes/modest_maze.txt")
+    for row in maze:
+        print(row)
